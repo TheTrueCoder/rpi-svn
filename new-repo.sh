4c43afea1d6b533c8db858578b9cde22cb9a98db
Nathan <nathanreynolds147@gmail.com>
2022-02-15 17:25:21 +1300

    Update new-repo.sh

--- new-repo.sh
+++ new-repo.sh
@@ -1,5 +1,7 @@
 NAME=$1
+URL="http://192.168.1.2/svn"
 
 sudo svnadmin create /mnt/extssd/svn_repos/$NAME
 sudo chown -R www-data:www-data /mnt/extssd/svn_repos/
+# svn mkdir $URL/$NAME/trunk $URL/$NAME/branches $URL/$NAME/tags -m "New project $NAME" --parents
 echo "Url is: http://192.168.1.2/svn/$NAME"
